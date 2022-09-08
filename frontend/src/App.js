8f42eac0b5a448c592b9a461cb4b54464b461c31
sundarsri04 <sundarsri04@gmail.com>
2022-09-08 11:25:01 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Spark</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
